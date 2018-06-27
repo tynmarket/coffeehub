9c61dc24eddce645b7bc226ad180c1022d7e08dc
Takehiko Shinkura <tynmarket@gmail.com>
2018-06-27 21:49:28 +0900

    Add generators config

---
+++ config/initializers/generators.rb
@@ -0,0 +1,7 @@
+Rails.application.config.generators do |g|
+  g.stylesheets false
+  g.javascripts false
+  g.helper false
+  g.template_engine false
+  g.test_framework :rspec, view_specs: false, helper_specs: false, controller_specs: false, routing_specs: false
+end
