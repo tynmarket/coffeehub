5197886a27215acd5e789f422fe6fb103995b8f1
Takehiko Shinkura <tynmarket@gmail.com>
2018-06-30 17:30:41 +0900

    Fix

--- db/fixtures/sites.rb
+++ db/fixtures/sites.rb
@@ -1,5 +1,5 @@
 Site.seed(:id,
-  {id: 1, name: "MUI", url: "http://www.mui-motosumi.co.jp"},
-  {id: 1, name: "ROKUMEI COFFEE CO.", url: "https://www.rococo-coffee.co.jp"},
-  {id: 1, name: "春木屋 月滴庵 “Gettekian”", url: "http://www.harukiya.com"},
+  {id: 1, name: "Mui", url: "http://www.mui-motosumi.co.jp"},
+  {id: 2, name: "ROKUMEI COFFEE CO.", url: "https://www.rococo-coffee.co.jp"},
+  {id: 3, name: "春木屋 月滴庵 “Gettekian”", url: "http://www.harukiya.com"},
 )
