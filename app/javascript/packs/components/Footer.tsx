62f2d69ec78024b263bc4c47599b471622bd1bff
Takehiko Shinkura <tynmarket@gmail.com>
2018-07-06 17:06:28 +0900

    Replace footer

--- app/javascript/packs/components/Footer.tsx
+++ app/javascript/packs/components/Footer.tsx
@@ -2,6 +2,23 @@ import * as React from "react";
 
 export const Footer: React.StatelessComponent<{}> = () => {
   return (
-    <div/>
+    <footer className="footer">
+      <p className="copyright">
+        ©
+        <a className="twitter-name" href="https://twitter.com/tynmarket" target="_blank">
+          tynmarket
+        </a>
+      </p>
+      <div className="copyright-flaticon">
+        Icons made by
+        <a href="http://www.freepik.com" title="Freepik" target="_blank">
+        &nbsp;Freepik&nbsp;
+        </a>
+        is licensed by
+        <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">
+          CC 3.0 BY
+        </a>
+      </div>
+    </footer>
   );
 };
