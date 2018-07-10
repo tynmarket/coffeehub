4f876e68bb52b39e16f364a5c896e757920a3568
Takehiko Shinkura <tynmarket@gmail.com>
2018-07-10 12:05:35 +0900

    Update babel settings

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,5 +1,6 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
+import "babel-polyfill";
 import { App } from "./components/App";
 
 document.addEventListener("DOMContentLoaded", () => {
