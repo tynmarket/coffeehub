44f977b356c1f3afd8c0af9abc9a293d42113f99
Takehiko Shinkura <tynmarket@gmail.com>
2018-07-28 13:19:43 +0900

    Add 珈琲きゃろっと

--- db/fixtures/sites.rb
+++ db/fixtures/sites.rb
@@ -2,4 +2,5 @@ Site.seed(:id,
   {id: 1, name: "Mui", url: "http://www.mui-motosumi.co.jp"},
   {id: 2, name: "ROKUMEI COFFEE CO.", url: "https://www.rococo-coffee.co.jp"},
   {id: 3, name: "春木屋 月滴庵 “Gettekian”", url: "http://www.harukiya.com"},
+  {id: 4, name: "珈琲きゃろっと", url: "https://coffeecarrot.jp/"},
 )
