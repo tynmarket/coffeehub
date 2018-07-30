2b0f38c474b982bc394b5a5128b15eeb76b8f52e
Takehiko Shinkura <tynmarket@gmail.com>
2018-07-31 04:25:00 +0900

    Add sg and subnet of db

--- terraform/security_group.tf
+++ terraform/security_group.tf
@@ -17,3 +17,23 @@ resource "aws_security_group" "web" {
     cidr_blocks     = ["0.0.0.0/0"]
   }
 }
+
+resource "aws_security_group" "db" {
+  name        = "db"
+  description = "db"
+  vpc_id      = "${aws_vpc.main.id}"
+
+  ingress {
+    from_port = "3306"
+    to_port = "3306"
+    protocol = "tcp"
+    security_groups = ["${aws_security_group.web.id}"]
+  }
+
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
+}
--- terraform/subnet.tf
+++ terraform/subnet.tf
@@ -6,3 +6,12 @@ resource "aws_subnet" "web" {
     Name = "tf-test-web"
   }
 }
+
+resource "aws_subnet" "db" {
+  vpc_id = "${aws_vpc.main.id}"
+  cidr_block = "10.1.2.0/24"
+
+  tags {
+    Name = "tf-test-db"
+  }
+}
