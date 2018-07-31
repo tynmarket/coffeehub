be187f1022a9cd7d0fb5bc34e075198c3b0c2038
Takehiko Shinkura <tynmarket@gmail.com>
2018-07-31 23:03:05 +0900

    Add route_table_association to db_sub

--- terraform/route_table_association.tf
+++ terraform/route_table_association.tf
@@ -3,7 +3,12 @@ resource "aws_route_table_association" "main" {
   route_table_id = "${aws_route_table.main.id}"
 }
 
-resource "aws_route_table_association" "nat_gateway" {
+resource "aws_route_table_association" "db" {
   subnet_id      = "${aws_subnet.db.id}"
+  route_table_id = "${aws_route_table.nat_gateway.id}"
+}
+
+resource "aws_route_table_association" "db_sub" {
+  subnet_id      = "${aws_subnet.db_sub.id}"
   route_table_id = "${aws_route_table.nat_gateway.id}"
 }
