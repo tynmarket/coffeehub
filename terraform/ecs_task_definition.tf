45edf128559b33a619f5c2212cc80031b70cdaeb
Takehiko Shinkura <tynmarket@gmail.com>
2018-08-04 03:08:47 +0900

    Add ecs_cluster

--- terraform/ecs_task_definition.tf
+++ terraform/ecs_task_definition.tf
@@ -1,6 +1,6 @@
 resource "aws_ecs_task_definition" "app" {
   family                = "nginx"
-  cpu = 256
-  memory = 300
+  cpu = "1 vCPU"
+  memory = 1024
   container_definitions = "${file("task-definition.json")}"
 }
