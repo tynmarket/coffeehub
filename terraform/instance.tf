7605460a3d2f870eff5d9ec7f904aa576528979c
Takehiko Shinkura <tynmarket@gmail.com>
2018-08-03 23:31:53 +0900

    Update security_group

--- terraform/instance.tf
+++ terraform/instance.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "web" {
   instance_type = "t2.micro"
   key_name = "awskey"
   subnet_id = "${aws_subnet.web.id}"
-  vpc_security_group_ids = ["${aws_security_group.web.id}"]
+  vpc_security_group_ids = ["${aws_security_group.app.id}", "${aws_security_group.ssh.id}"]
 
   tags {
     Name = "tf-test"
--- terraform/security_group.tf
+++ terraform/security_group.tf
@@ -1,6 +1,47 @@
+# TODO egress
 resource "aws_security_group" "web" {
   name        = "web"
   description = "web"
+  vpc_id      = "${aws_vpc.main.id}"
+
+  ingress {
+    from_port = "80"
+    to_port = "80"
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "app" {
+  name        = "app"
+  description = "app"
+  vpc_id      = "${aws_vpc.main.id}"
+
+  ingress {
+    from_port = "80"
+    to_port = "80"
+    protocol = "tcp"
+    security_groups = ["${aws_security_group.web.id}"]
+  }
+
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "ssh" {
+  name        = "ssh"
+  description = "ssh"
   vpc_id      = "${aws_vpc.main.id}"
 
   ingress {
@@ -27,7 +68,7 @@ resource "aws_security_group" "db" {
     from_port = "3306"
     to_port = "3306"
     protocol = "tcp"
-    security_groups = ["${aws_security_group.web.id}"]
+    security_groups = ["${aws_security_group.app.id}"]
   }
 
   egress {
