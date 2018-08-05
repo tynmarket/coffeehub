25338110113c0fe23bf4828f001162e58e3dc59d
Takehiko Shinkura <tynmarket@gmail.com>
2018-08-05 10:28:54 +0900

    Rename

--- terraform/ecs_task_definition.tf
+++ terraform/ecs_task_definition.tf
@@ -2,5 +2,5 @@ resource "aws_ecs_task_definition" "app" {
   family                = "nginx"
   cpu = "1 vCPU"
   memory = 500
-  container_definitions = "${file("task-definition.json")}"
+  container_definitions = "${file("container-definitions.json")}"
 }
