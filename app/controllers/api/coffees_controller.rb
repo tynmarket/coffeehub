d9f03779ed0cd68d61b3f3e2205c05ec47d29ec8
Takehiko Shinkura <tynmarket@gmail.com>
2018-08-10 21:18:36 +0900

    Add comment

--- app/controllers/api/coffees_controller.rb
+++ app/controllers/api/coffees_controller.rb
@@ -1,13 +1,13 @@
 module Api
   class CoffeesController < ApplicationController
     def index
-      sleep 0.1
+      sleep 0.1 # Wait for UX
       @coffees = Coffee.for_api(params[:page])
       render json: @coffees, each_serializer: CoffeeSerializer
     end
 
     def roast
-      sleep 0.1
+      sleep 0.1 # Wait for UX
       @coffees = Coffee.send(params[:roast]).for_api(params[:page])
       render json: @coffees, each_serializer: CoffeeSerializer
     end
