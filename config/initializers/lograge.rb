e0dc5e0424158f902100e4a97a60a71470543003
Takehiko Shinkura <tynmarket@gmail.com>
2018-08-20 23:10:11 +0900

    Change log formatter

--- config/initializers/lograge.rb
+++ config/initializers/lograge.rb
@@ -1,3 +1,7 @@
-Rails.application.config.lograge.custom_options = lambda do |event|
-  { time: event.time }
+Rails.application.configure do
+  config.lograge.formatter = Lograge::Formatters::Json.new
+
+  config.lograge.custom_options = lambda do |event|
+    { time: event.time }
+  end
 end
