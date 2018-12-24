7359c72630c35b71ca4e75a1f9b46cc9c625e136
Takehiko Shinkura <tynmarket@gmail.com>
2018-12-24 16:50:18 +0900

    Output webpack config

--- config/webpack/development.js
+++ config/webpack/development.js
@@ -2,4 +2,8 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'development'
 
 const environment = require('./environment')
 
+// const config = environment.toWebpackConfig()
+// const util = require('util')
+// console.log(util.inspect(config, false, null, true /* enable colors */))
+
 module.exports = environment.toWebpackConfig()
