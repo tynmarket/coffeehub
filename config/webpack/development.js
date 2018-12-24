c1c7fa5d883810bff904042791c99c8ba749e75e
Takehiko Shinkura <tynmarket@gmail.com>
2018-12-24 16:57:26 +0900

    Add link on comment

--- config/webpack/development.js
+++ config/webpack/development.js
@@ -6,4 +6,8 @@ const environment = require('./environment')
 // const util = require('util')
 // console.log(util.inspect(config, false, null, true /* enable colors */))
 
+// https://inside.pixiv.blog/subal/4615
+// https://medium.com/studist-dev/goodbye-webpacker-183155a942f6
+// https://qiita.com/mizchi/items/2e614d184fe2577f8256
+
 module.exports = environment.toWebpackConfig()
