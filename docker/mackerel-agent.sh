911a02c0e03cc2c4cff59535c26f562095c39ecc
Takehiko Shinkura <tynmarket@gmail.com>
2018-12-31 13:29:31 +0900

    Add docker-run command

--- docker/mackerel-agent.sh
+++ docker/mackerel-agent.sh
@@ -4,5 +4,6 @@ LOGFILE=/var/log/mackerel-agent.log
 PIDFILE=/var/run/mackerel-agent.pid
 
 cd mackerel-agent
+echo 'roles = [ "CoffeeHub:app" ]' >> mackerel-agent.conf
 echo "apikey = \"$MACKEREL_API_KEY\"" >> mackerel-agent.conf
 ./mackerel-agent supervise -conf mackerel-agent.conf --pidfile=$PIDFILE >>$LOGFILE 2>&1 &
