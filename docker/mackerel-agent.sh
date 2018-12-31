7c713335e75d71f213a178859a8000f5c6930ffc
Takehiko Shinkura <tynmarket@gmail.com>
2018-12-31 11:35:44 +0900

    Use alpine

---
+++ docker/mackerel-agent.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+LOGFILE=/var/log/mackerel-agent.log
+PIDFILE=/var/run/mackerel-agent.pid
+
+cd mackerel-agent
+echo "apikey = \"$MACKEREL_API_KEY\"" >> mackerel-agent.conf
+./mackerel-agent supervise -conf mackerel-agent.conf --pidfile=$PIDFILE >>$LOGFILE 2>&1 &
